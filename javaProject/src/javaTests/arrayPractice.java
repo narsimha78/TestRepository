d6787c672c10c5291baed7d10bf4483463f20185
narsimha78 <narsimha78@users.noreply.github.com>
2015-09-19 14:31:35 +1000

    fiveth change

--- javaProject/src/javaTests/arrayPractice.java
+++ javaProject/src/javaTests/arrayPractice.java
@@ -7,7 +7,7 @@ public class arrayPractice {
 		//Intialize an Array with Integers data
 		int a[]={2,4,8,7,3,5};
 		int i=0;
-		
+		//For loop to iterate through all the array list to find i index
 		for(i=0; i<a.length; i++)
 		{
 			
