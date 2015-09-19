90b27116d52a999f463230d8ffc3e8709001c581
narsimha78 <narsimha78@gmail.com>
2015-09-19 13:54:03 +1000

    first commit

---
+++ javaProject/src/javaTests/arrayPractice.java
@@ -0,0 +1,30 @@
+package javaTests;
+
+public class arrayPractice {
+
+	public static void main(String[] args) {
+
+		int a[]={2,4,8,7,3,5};
+		int i=0;
+		
+		for(i=0; i<a.length; i++)
+		{
+			
+			if(a[i]==7)
+			{
+				//x++;
+				System.out.println(i);
+
+				break;
+			}
+//			else
+//			{
+//				break;
+//			}
+			
+		}
+		
+		//System.out.println(i);
+	}
+
+}
